80c294a22092f18f7bd85ac53a1f1cfe743676ee
David Wendt (kmeisthax) <dcrkid@yahoo.com>
2009-10-08 23:36:55 -0400

    Added wiienv.c, which will provide functions useful for determining how a program was loaded and what kind of Wii it was loaded on
    Later on I should move some of the AnyTitle Deleter functions into this

---
+++ source/wiienv.c
@@ -0,0 +1,50 @@
+/*-------------------------------------------------------------
+ 
+wiienv.c - obtain information from the Wii environment
+
+Copyright (C) 2009 kmeisthax
+Unless other credit specified
+ 
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any
+damages arising from the use of this software.
+ 
+Permission is granted to anyone to use this software for any
+purpose, including commercial applications, and to alter it and
+redistribute it freely, subject to the following restrictions:
+ 
+1.The origin of this software must not be misrepresented; you
+must not claim that you wrote the original software. If you use
+this software in a product, an acknowledgment in the product
+documentation would be appreciated but is not required.
+ 
+2.Altered source versions must be plainly marked as such, and
+must not be misrepresented as being the original software.
+ 
+3.This notice may not be removed or altered from any source
+distribution.
+ 
+-------------------------------------------------------------*/
+
+#include "wiienv.h"
+#include "wiicontents.h"
+
+//might as well be include <vulnerabilities.h>
+#include <string.h>
+
+//useful addresses
+#define STUBHAXX_PTR 0x80001804
+#define TID_LOWER_PTR  0x80003180
+
+s32 __runningFromLoader(int* trueIfLoader) {
+    //Check for the presence of "STUBHAXX" at STUBHAXX_PTR
+    char* stubhaxx = (char*)STUBHAXX_PTR;
+    char* testhaxx = "STUBHAXX";
+    
+    if (memcmp(stubhaxx, testhaxx, 8) == 0)
+        *trueIfLoader = true;
+    else
+        *trueIfLoader = false;
+        
+    return WCT_OKAY;
+}
