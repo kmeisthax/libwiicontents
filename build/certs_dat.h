63fce99c8b4d597ab05c3fc008444a6bcf5e518f
David Wendt (kmeisthax) <dcrkid@yahoo.com>
2009-10-08 23:31:07 -0400

    Removed build/ folder (cleaning up the source tree)

--- build/certs_dat.h
+++
@@ -1,3 +0,0 @@
-extern const u8 certs_dat_end[];
-extern const u8 certs_dat[];
-extern const u32 certs_dat_size;
--- build/fake_su_ticket_dat.h
+++
@@ -1,3 +0,0 @@
-extern const u8 fake_su_ticket_dat_end[];
-extern const u8 fake_su_ticket_dat[];
-extern const u32 fake_su_ticket_dat_size;
